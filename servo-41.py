17e32d86e90b13e6a86c2c9abff4e526a0548119
arrowsmith001 <arrowsmith001dev@gmail.com>
2024-12-01 23:39:14 +0000

    removed redundant files, tidied code

--- servo-41.py
+++ servo-41.py
@@ -3,32 +3,25 @@
 from Adafruit_PWM_Servo_Driver import PWM
 import time
 
-# ===========================================================================
 
-# Example Code
-# ===========================================================================
+pwm = PWM(0x41, debug=True) # Toggle between 0x40 and 0x41 for U2 and U3
 
-# Initialise the PWM device using the default address
-# bmp = PWM(0x40, debug=True)
-pwm = PWM(0x41, debug=True)
-
-servoMin = 102  # Min pulse length out of 4096 (1 ms)
-servoMid = 307  # Mid pulse length out of 4096 (1.5 ms)
-servoMax = 512  # Max pulse length out of 4096 (2 ms)
+servoMin = 102  # 0 degrees
+servoMid = 307  # 90 degrees
+servoMax = 512  # 180 degrees
 
 def setServoPulse(channel, pulse):
-  pulseLength = 1000000                   # 1,000,000 us per second
-  pulseLength /= 50                       # 50 Hz
+  pulseLength = 1000000                   
+  pulseLength /= 50                       
   print ("%d us per period" % pulseLength)
-  pulseLength /= 4096                     # 12 bits of resolution
+  pulseLength /= 4096                     
   print ("%d us per bit" % pulseLength)
   pulse *= 1000
   pulse /= pulseLength
   pwm.setPWM(channel, 0, pulse)
 
-pwm.setPWMFreq(50)                        # Set frequency to 50 Hz
+pwm.setPWMFreq(50)                        
 while (True):
-  # Change speed of continuous servo on channel 0
   pwm.setPWM(15, 0, servoMin)
   time.sleep(0.5)
   pwm.setPWM(15, 0, servoMid)
